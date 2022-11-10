f929370d08b2b4ed8bdd563c2c21cb2db63a7172
saranfordev <ken.ball191@gmail.com>
2022-11-10 17:12:57 +0700

    My-Portfolio-Header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,6 +3,7 @@ import './header.css'
 import CTA from './CTA'
 import ME from '../../assets/me.png'
 import HeaderSocials from './HeaderSocials'
+
 const Header = () => {
   return (
     <section>
@@ -12,10 +13,11 @@ const Header = () => {
         <h5 className="text-light">Frontend Development</h5>
         <CTA/>
         <HeaderSocials/>
-        <div className="me">
-          <img src={ME} alt="me" />
-        </div>
 
+          <div className='me'>
+              <img src={ME} alt="me" />
+          </div>
+        
         <a href="#contact" className='scroll__down'>Scroll Down</a>
       </div>
     </section>
