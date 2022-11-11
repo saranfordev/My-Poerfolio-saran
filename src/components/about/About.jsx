57833594040c16eb2060458d1902181a2a647e4e
saranfordev <ken.ball191@gmail.com>
2022-11-11 16:49:02 +0700

    My-Portfolio-Experience

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,8 +1,55 @@
 import React from 'react'
 import './about.css'
+import ME from '../../assets/me-about.jpg'
+import {FiAward} from 'react-icons/fi'
+import {HiOutlineUsers} from 'react-icons/hi'
+import {AiFillFolderOpen} from 'react-icons/ai'
+
 const About = () => {
   return (
-    <section id='about'>About</section>
+    <section id='about'>
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="aboutImage" />
+          </div>
+        </div>
+
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <FiAward className='about__icon'/>
+              <h5>Experience</h5>
+              <small>2 Months Studying</small>
+            </article>
+          
+            <article className="about__card">
+              <HiOutlineUsers className='about__icon'/>
+              <h5>Clients</h5>
+              <small>200+ Worldwide</small>
+            </article>
+          
+            <article className="about__card">
+              <AiFillFolderOpen className='about__icon'/>
+              <h5>Projects</h5>
+              <small>80+ Completed</small>
+            </article>
+          </div>
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Rem voluptatibus numquam earum, placeat iure magnam perspiciatis ex incidunt voluptates alias. 
+            Iste deleniti distinctio nostrum perferendis laborum necessitatibus? Tenetur, 
+            adipisci perferendis?
+          </p>
+
+          <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+        </div>
+      </div>
+    </section>
   )
 }
 
--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,8 +1,102 @@
 import React from 'react'
 import './experience.css'
+import {BsCheckCircleFill} from 'react-icons/bs'
+
 const Experience = () => {
   return (
-    <section id='experience'>Experience</section>
+    <section id='experience'>
+      <h5>What Skills I Have</h5>
+      <h2>My Experience</h2>
+
+      <div className="container experience__container">
+
+        <div className="experience__frontend">
+          <h3>Frotend Development</h3>
+          <div className="experience__content">
+            <article className='experience__details'>
+              <BsCheckCircleFill className='experience__details-icon'/>
+              <div>
+                <h4>HTML</h4>
+                <small className='text-light'>Basic</small>
+              </div>
+            </article>
+
+            <article className='experience__details'>
+              <BsCheckCircleFill className='experience__details-icon'/>
+              <div>
+                <h4>CSS</h4>
+                <small className='text-light'>Basic</small>
+              </div>
+            </article>
+
+            <article className='experience__details'>
+              <BsCheckCircleFill className='experience__details-icon'/>
+              <div>
+                <h4>Javascript</h4>
+                <small className='text-light'>Basic</small>
+              </div>
+            </article>
+
+            <article className='experience__details'>
+              <BsCheckCircleFill className='experience__details-icon'/>
+              <div>
+                <h4>Bootstrap</h4>
+                <small className='text-light'>Basic</small>
+              </div>
+            </article>
+
+            <article className='experience__details'>
+              <BsCheckCircleFill className='experience__details-icon'/>
+              <div>
+                <h4>Vue.js</h4>
+                <small className='text-light'>Basic</small>
+              </div>
+            </article>
+
+            <article className='experience__details'>
+              <BsCheckCircleFill className='experience__details-icon'/>
+              <div>
+                <h4>React.js</h4>
+                <small className='text-light'>Basic</small>
+              </div>
+            </article>
+          </div>
+        </div>
+
+        {/* END OF FRONTEND */}
+
+        <div className="experience__backend">
+          <h3>Backend Development</h3>
+          <div className="experience__content">
+            <article className='experience__details'>
+              <BsCheckCircleFill className='experience__details-icon'/>
+              <div>
+                <h4>Premiere</h4>
+                <small className='text-light'>Basic</small>
+              </div>
+            </article>
+
+            <article className='experience__details'>
+              <BsCheckCircleFill className='experience__details-icon'/>
+              <div>
+                <h4>Photoshop</h4>
+                <small className='text-light'>Basic</small>
+              </div>
+            </article>
+
+            <article className='experience__details'>
+              <BsCheckCircleFill className='experience__details-icon'/>
+              <div>
+                <h4>Git</h4>
+                <small className='text-light'>Basic</small>
+              </div>
+            </article>
+          </div>
+
+        </div>
+
+      </div>
+    </section>
   )
 }
 
